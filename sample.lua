29696e87dfd2bf3f460591e46f9cfbde82253c30
东风有意 <791141513@qq.com>
2020-11-07 14:07:42 +0800

    init

--- sample.lua
+++ sample.lua
@@ -1,6 +1,6 @@
-local Linq = require('MyLinq')
+local Linq = require('Linq')
 Linq.Repeat(function(v) return math.random(1,100) end, 20)
-    :ForEach(print)
+    :Where(function(v) return v>20 end)
     :TakeByOrder(10, function(v) return -v end)
     :Select(function(v)
         return v+1000
