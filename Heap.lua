f8f05b8d3706fb38dac00217294af21a0f7cbf78
东风有意 <791141513@qq.com>
2020-11-07 14:02:29 +0800

    init

---
+++ Heap.lua
@@ -0,0 +1,44 @@
+local Heap = {cmp = function(a,b) return a < b end}
+
+function Heap:Push(elem)
+    if #self < self.size then -- 堆未满，将新元素加入堆
+        self[#self+1] = elem
+        if #self == self.size then  --堆满刚时进行一次排序
+        	table.sort(self, function(a,b) return self.cmp(a,b) end)
+        	for i=1,math.floor(#self/2) do --逆序
+        		local i2 = #self+1-i
+        		self[i], self[i2] = self[i2], self[i]
+        	end
+        end
+    elseif self.cmp(elem, self[1]) then --堆已满，比较新元素与堆顶。若新元素更优：新元素替换堆顶，更新堆以保证堆顶为最劣元素
+        self[1] = elem
+        self:Update(1)
+    end
+    return self
+end
+
+function Heap:Update(index)
+    local left = index * 2
+    if left > self.size then return end
+    local right = left + 1
+    local childIndex = right <= self.size and self.cmp(self[left], self[right]) and right or left --较劣的子元素
+
+    if self.cmp(self[index], self[childIndex]) then --新元素更优，交换
+        self[index], self[childIndex] = self[childIndex], self[index]
+        self:Update(childIndex)
+    end
+end
+
+function Heap:Done()
+    table.sort(self, self.cmp)
+    return self
+end
+
+return function(size, cmp)
+    return setmetatable({
+        size = size,
+        cmp = cmp
+    }, {
+        __index = Heap
+    })
+end
